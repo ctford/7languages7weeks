edc2e557ca6d6a38078d19d095ddac54ce941b70
Chris Ford <christophertford@gmail.com>
2011-06-12 20:58:45 +0100

    Completed the first day's exercises (except for the bonus).

---
+++ ruby/miscellaneous.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+# Print the string "Hello, world."
+puts "Hello, world."
+
+# For the string "Hello, Ruby", find the index of the word "Ruby".
+puts "Hello, Ruby".index "Ruby"
+
+# Print your name ten times.
+10.times {puts "ctford"}
+
+# Print the string "This is sentence number x", where x changes from 1 to 10.
+(1..10).each {|x| puts x}
