19b5aa6e0ce67da86d4dce3589fe83c6db9303e7
Chris Ford <christophertford@gmail.com>
2011-06-19 16:47:34 +0100

    Checks for horizontal wins only.

--- scala/tic-tac-toe.scala
+++ scala/tic-tac-toe.scala
@@ -1,10 +1,25 @@
-class Board(val top: Boolean) {
-	def win():Boolean = top
+class Board(
+	val top: (Char, Char, Char),
+	val middle: (Char, Char, Char),
+	val bottom: (Char, Char, Char)
+) {
+	def wins(marker:Char):Boolean = lines().filter(line => win(marker, line)).size > 0 
+
+	def lines():List[(Char, Char, Char)] = List(top, middle, bottom)
+
+	def win(marker:Char, line:(Char, Char, Char)):Boolean = line match {
+		case (a, b, c) => a == marker && b == marker && c == marker
+	}
 }
 
 
-val board = new Board(true)
+val board = new Board(
+	('X', 'X', 'X'), 
+	('X', 'X', 'X'), 
+	('X', 'X', 'X') 
+)
+val winner = if(board.wins('X')) "X"
+	else if(board.wins('O')) "O"
+	else "no one"
 
-val message = board match {
-	case b if b.win() => "X wins!"
-}
+println("The result is that " + winner + " wins.")
