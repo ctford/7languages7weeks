cdf84a872b9fa2d17055c5c168cb8457122e4249
Chris Ford <christophertford@gmail.com>
2011-06-19 17:02:41 +0100

    Implemented horizontals, verticals and diagonals.

--- scala/tic-tac-toe.scala
+++ scala/tic-tac-toe.scala
@@ -5,21 +5,31 @@ class Board(
 ) {
 	def wins(marker:Char):Boolean = lines().filter(line => win(marker, line)).size > 0 
 
-	def lines():List[(Char, Char, Char)] = List(top, middle, bottom)
-
 	def win(marker:Char, line:(Char, Char, Char)):Boolean = line match {
 		case (a, b, c) => a == marker && b == marker && c == marker
 	}
+
+	def lines():List[(Char, Char, Char)] = {
+		val (a, b, c) = top
+		val (d, e, f) =	middle 
+		val (h, i, j) = bottom 
+
+		val verticals = List((a, d, h), (b, e, i), (c, f, j)) 
+		val horizontals = List(top, middle, bottom)
+		val diagonals = List((a, e, j), (h, e, c))
+
+		horizontals ++ verticals ++ diagonals
+	}
 }
 
-
 val board = new Board(
-	('X', 'X', 'X'), 
-	('X', 'X', 'X'), 
-	('X', 'X', 'X') 
+	('X', '_', 'O'), 
+	('_', 'O', '_'), 
+	('O', 'X', 'X') 
 )
-val winner = if(board.wins('X')) "X"
-	else if(board.wins('O')) "O"
+
+val winner = if(board.wins('X')) "'X'"
+	else if(board.wins('O')) "'O'"
 	else "no one"
 
 println("The result is that " + winner + " wins.")
